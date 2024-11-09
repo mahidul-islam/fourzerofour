6cb5df0665ab1ab1427e39b3901f3a6121556df8
mahidul-islam <mizihan84@gmail.com>
2024-11-09 22:01:39 +0600

    added hunter is chasing hunted crude ly

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: constant_identifier_names
 
 import 'dart:async';
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:get/get.dart';
@@ -8,7 +9,7 @@ import 'package:rive/rive.dart';
 
 const FRAME_PER_SECOND = 30;
 const HUNTER_SPEED = 5;
-const HUNTED_SPEED = 15;
+const HUNTED_SPEED = 10;
 
 class HomeController extends GetxController {
   SMIInput<bool>? hunterKlick;
@@ -35,8 +36,8 @@ class HomeController extends GetxController {
     hunterLeft.value = 20;
     hunterTop.value = 20;
 
-    huntedLeft.value = 20;
-    huntedTop.value = 20;
+    huntedLeft.value = Get.width - 120;
+    huntedTop.value = Get.height - 180;
     timer = Timer.periodic(
         const Duration(milliseconds: 1000 ~/ FRAME_PER_SECOND), (Timer timer) {
       updateHunterPosition(100);
@@ -118,8 +119,60 @@ class HomeController extends GetxController {
 
   void updateHuntedPosition(double containerWidth) {
     final Size size = Get.size;
+    final double scareDistance = size.width / 3;
 
-    huntedLeft.value = (huntedLeft.value) + (HUNTED_SPEED * huntedDirectionX);
-    huntedTop.value = (huntedTop.value) + (HUNTED_SPEED * huntedDirectionY);
+    final double safeDistance = 2 * size.width / 3;
+
+    // Calculate distance between hunter and hunted
+    double dx = hunterLeft.value - huntedLeft.value;
+    double dy = hunterTop.value - huntedTop.value;
+    double distance = sqrt(dx * dx + dy * dy);
+
+    // If hunter is too far, stop flee
+    if (distance > safeDistance) {
+      triggerHuntedKlick();
+      return;
+    }
+
+    // If hunter is too close, adjust direction to flee
+    if (distance < scareDistance) {
+      // Calculate unit vector pointing from hunter to hunted
+      double unitX = dx / distance;
+      double unitY = dy / distance;
+
+      // Set direction opposite to hunter's position
+      huntedDirectionX = unitX > 0 ? -1 : 1;
+      huntedDirectionY = unitY > 0 ? -1 : 1;
+
+      // Trigger scared animation if implemented
+      triggerHuntedKlick();
+    }
+
+    // Check screen boundaries and bounce
+    if (huntedLeft.value > (size.width - containerWidth)) {
+      if (huntedDirectionX == 1) {
+        huntedDirectionX = -1;
+      }
+    }
+    if (huntedLeft.value < 0) {
+      if (huntedDirectionX == -1) {
+        huntedDirectionX = 1;
+      }
+    }
+
+    if (huntedTop.value > (size.height - containerWidth)) {
+      if (huntedDirectionY == 1) {
+        huntedDirectionY = -1;
+      }
+    }
+    if (huntedTop.value < 0) {
+      if (huntedDirectionY == -1) {
+        huntedDirectionY = 1;
+      }
+    }
+
+    // Update position with current direction and speed
+    huntedLeft.value = huntedLeft.value + (HUNTED_SPEED * huntedDirectionX);
+    huntedTop.value = huntedTop.value + (HUNTED_SPEED * huntedDirectionY);
   }
 }
